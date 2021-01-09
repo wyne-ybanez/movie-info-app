c46602ba7f62c82f6f571dc9d01d8de65edc6d30
wyne-ybanez <wyne_01@icloud.com>
2021-01-09 01:09:39 +0000

    JS code updated to get API information and display it on the page. Altering HTML using jquery once an input has been submitted

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Movie Info App
 
+Incomplete project - will need to sign up to a movie info free API 
+In the project I am using someone else's API 
+With a valid the app is expected to function as intended
+
 ## Acknowledgements
 
 - Brad Traversy
 - Youtube link to tutorial: https://www.youtube.com/watch?v=YsPqjYGauns&t=290s&ab_channel=TraversyMedia
+- Emmanuel Semutenga - programmer who owns the API key
--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $(document).ready(() => {
 function getMovies(searchText) {
 
     // communicate w axios API, if okay response will run
-    // using someone else's API key
+    // not my API key
     axios.get("https://api.themoviedb.org/3/search/movie?api_key=98325a9d3ed3ec225e41ccc4d360c817&language=en-US&query=" + searchText)
         .then((response) => {
             console.log(response)
@@ -27,13 +27,66 @@ function getMovies(searchText) {
                         <div class="well text-center">
                             <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
                             <h5>${movie.title}</h5>
-                            <a onclick="movieSelected('${movie.id}')" class="btn btn-secondary" href="#"> Movie Details </a>
+                            <a onclick="movieSelected('${movie.id}')" class="btn btn-secondary m-1 p-2" href="#">Movie Details</a>
                         </div>
                     </div>
                     `
             })
 
             $('#movies').html(output)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
+
+// save to local storage
+function movieSelected(id) {
+
+    // get key 'movieId' and set it to the 'id' value
+    sessionStorage.setItem('movieId', id)
+    window.location = 'movie.html'
+    return false
+}
+
+function getMovie() {
+    // get movie Id from local storage
+    let movieId = sessionStorage.getItem('movieId')
+
+    // not my API key - will need permission
+    axios.get("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=98325a9d3ed3ec225e41ccc4d360c817")
+        .then((response) => {
+            console.log(response)
+
+            let movie = response.data //response of the api regarding 'data' object
+            let output = `
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="${movie.poster_path}" class="thumbnail">
+                    </div>
+                    <div class="col-md-8">
+                        <h2>${movie.title}</h2>
+                        <ul class="list-group">
+                            <li class="list-group-item"><strong>Genre:</strong> ${movie.genres[0].name}, ${movie.genres[1].name}</li>
+                            <li class="list-group-item"><strong>Released:</strong> ${movie.release_date}</li>
+                            <li class="list-group-item"><strong>Rated:</strong> ${movie.vote_average}</li>
+                            <li class="list-group-item"><strong>Runtime:</strong> ${movie.runtime}</li>
+                            <li class="list-group-item"><strong>Popularity:</strong> ${movie.popularity}</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="well">
+                        <h3>Plot:</h3>
+                        ${movie.overview}
+                        <hr>
+                        <a href="http://imdb.com/title/${movie.imdb_id}" target="_blank" class="btn btn-primary">View IMDB</a>
+                        <a href="index.html" class="btn btn-default">Go Back To Search</a>
+                    </div>
+                </div>
+            `
+
+            $('#movie').html(output);
         })
         .catch((error) => {
             console.log(error)
